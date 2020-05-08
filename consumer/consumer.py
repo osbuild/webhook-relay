73cc4b4122e81eebedb3a485f87276d9cb944aec
Major Hayden <major@redhat.com>
2020-05-08 14:55:36 -0500

    Remove exception handling

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -16,31 +16,27 @@ queue = sqs.get_queue_by_name(QueueName=SQS_QUEUE)
 print("Webhook consumer starting up!")
 
 while True:
-    try:
-        for message in queue.receive_messages():
-            print(f"Got message: {message.message_id}")
-            parsed = json.loads(message.body)
-            original_headers = parsed['headers']
-            payload = parsed['payload']
+    for message in queue.receive_messages():
+        print(f"Got message: {message.message_id}")
+        parsed = json.loads(message.body)
+        original_headers = parsed['headers']
+        payload = parsed['payload']
 
-            # Set up our headers.
-            headers = {}
-            headers['Content-Type'] = "application/json"
-            headers['X-GitHub-Event'] = "push"
+        # Set up our headers.
+        headers = {}
+        headers['Content-Type'] = "application/json"
+        headers['X-GitHub-Event'] = "push"
 
-            # Post the message to jenkins.
-            resp = requests.post(
-                JENKINS_URL,
-                headers=headers,
-                data=json.dumps(payload),
-                verify=False
-            )
-            print(resp.text)
+        # Post the message to jenkins.
+        resp = requests.post(
+            JENKINS_URL,
+            headers=headers,
+            data=json.dumps(payload),
+            verify=False
+        )
+        print(resp.text)
 
-            # Delete the message if we made it this far.
-            message.delete()
-
-    except Exception:
-        pass
+        # Delete the message if we made it this far.
+        message.delete()
 
     time.sleep(15)
